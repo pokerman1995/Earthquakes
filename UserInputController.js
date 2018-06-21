0d3b238c467c8353caa4b319b91f11e4a01fa0d1
Philipp Sutor <p.sutorx@gmail.com>
2018-06-21 13:37:12 +0200

    InputHandling
    Anlegen Eventpublisher zur einfachen Verarbeitung von Userinput. UserInputController hört auf userinput und Hauptklasse kann bei fertigstellung der Methoden auf diese Reagieren.

--- UserInputController.js
+++ UserInputController.js
@@ -1,1 +1,42 @@
+var d3 = d3 || {}
+var Earthquakes = Earthquakes || {}
+
+Earthquakes.UserInputController = function () {
+	"use strict";
+
+  var that = new EventPublisher(),
+    controls = document.querySelector(".menue");
+
+
+  function onFearButtonClicked() {
+    that.notifyAll("changeLayoutToGeneralFear", null);
+  }
+
+  function onWitnessButtonClicked() {
+    that.notifyAll("changeLayoutToWitnessedEarthquakes", null);
+  }
+
+  function onFatalButtonClicked() {
+    that.notifyAll("changeLayoutToFatalFear", null);
+  }
+
+
+  function init() {
+    var button1 = controls.querySelector(".fear-button"),
+     button2 = controls.querySelector(".witness-button"),
+     button3 = controls.querySelector(".fatal-button");
+
+    button1.addEventListener("click", onFearButtonClicked);
+    button2.addEventListener("click", onWitnessButtonClicked);
+    button3.addEventListener("click", onFatalButtonClicked);
+  }
+
+  init();
+
+  that.onFearButtonClicked = onFearButtonClicked;
+  that.onWitnessButtonClicked = onWitnessButtonClicked;
+  that.onFatalButtonClicked = onFatalButtonClicked;
+  return that;
+}
+
 
